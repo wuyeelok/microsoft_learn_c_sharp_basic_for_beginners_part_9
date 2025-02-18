d0129ab0711f167be892af4a22700fa528f28b2e
wuyeelok <wuyeelok@gmail.com>
2025-02-18 11:24:01 +0800

    basic if else statement

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 var part = "Part 9";
 Console.WriteLine($"Hello, World! {part}");
+
+int a = 5;
+int b = 6;
+
+
+int c = a + b;
+
+bool isGreaterThan10 = c > 10; // will be true or false
+
+if (isGreaterThan10) // true
+{
+    Console.WriteLine("The sum is greater than 10.");
+}
+else // false
+{
+    Console.WriteLine("The sum is not greater than 10.");
+}
